702488f696e7335a292cc742f1d09512e9287845
Stephanos Malikotsinas <31777077+circular-code@users.noreply.github.com>
2018-03-30 18:44:12 +0200

    fisher yates shuffle

--- utils.js
+++ utils.js
@@ -162,6 +162,19 @@ var utils = {
                 }
             }
         return q.join("&");
-    }
+    },
+    /* fisher-yates shuffle */
+    shuffle: function(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    },
 };
 
