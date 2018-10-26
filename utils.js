4ba546a5f43d0092d64d58a286f390f71b1019f2
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2018-10-26 15:52:25 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -11,35 +11,6 @@ var utils = {
         }
         return Math.floor(Math.random()*(max-min)+min);
     },
-    generateData: function(count) {
-        var i;
-        var status = ['New', 'Canceled', 'Processing', 'Rejected', 'Approved', 'Work', 'Ready to Print', 'Printing', 'Printed', 'Diecut', 'Error'];
-        var bereich = ['Std', 'RP', 'VK', 'EK', 'WMF', 'EST', 'DTI', 'DPT', 'DTS', 'CPC', 'MSI'];
-        var batchName = ['In_Plo_ha', 'In_Hi', 'In_Da_te', 'In_Duk_nam','In_Zu','In_Ter_set','In_Dev_tx','In_In_out','In_In','In_In_in','In_Dx_tx'];
-        var lack = ['Matt', 'Gloss'];
-        var items = [],
-            startDate = Date.parse('1/1/1975'),
-            endDate = Date.parse('1/1/1992');
-        for (i = 0; i < count; i++) {
-            var date = new Date(utils.randomNumBetween(endDate, startDate));
-            date.setHours(12);
-            var item = {
-                id: i + 1,
-                Status: status[utils.randomNumBetween(status.length)],
-                Bereich: bereich[utils.randomNumBetween(bereich.length)],
-                BatchId: utils.randomNumBetween(3000,2000),
-                BatchName: batchName[utils.randomNumBetween(batchName.length)],
-                AnzahlJobs: utils.randomNumBetween(100),
-                Auflage: utils.randomNumBetween(300),
-                AnzahlBogen: utils.randomNumBetween(5000),
-                Substrat: 'Ensocoat-' +utils.randomNumBetween(3000),
-                Lack: lack[utils.randomNumBetween(lack.length)] + ' ' + utils.randomNumBetween(400000,200000),
-                Erstelldatum: moment(date).format('L')
-            };
-            items.push(item);
-        }
-        return items;
-     },
      /**
      * Transforms URL Strings to Javascript Object; currently only works with flat objects.
      * @param {string} urlString URL String Parameter in the form of "abc=foo&def=%5Basf%5D&xyz=5"
