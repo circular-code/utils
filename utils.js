799cd9c058d049f6998b0852eeddd95c3ddb0140
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2019-05-23 11:21:43 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -343,7 +343,39 @@ var utils = {
                 return ' ' + mimeTypesString.split('/')[1].toUpperCase();
             else
                 return '';
-        }
+        },
+    },
+    convertNormalNumberToRomanNumber: function (n) {
+	var romanNumbersLookup = {
+	    '1': 'I',
+	    '4': 'IV',
+	    '5': 'V',
+	    '9': 'IX',
+	    '10': 'X',
+	    '40': 'XL',
+	    '50': 'L',
+	    '90': 'XC',
+	    '100': 'C',
+	    '400': 'CD',
+	    '500': 'D',
+	    '900': 'CM',
+	    '1000': 'M',
+	};
+
+	var romanNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+	var i = 0;
+	var string = '';
+
+	while (i < 13) {
+		if (n - romanNumbers[i] < 0) {
+			i++;
+		}
+		else {
+			string += romanNumbersLookup[romanNumbers[i]];
+			n = n - romanNumbers[i];
+		}
+	}
+	return string;
     }
 };
 
